3c272aa3b2ab319dcf35d2857271572ad31f1e90
Takuma HANATANI <mail@potato4d.me>
2019-12-03 20:38:37 +0900

    chore: Add IsString

--- day3-dto-and-validation/src/items/items.dto.ts
+++ day3-dto-and-validation/src/items/items.dto.ts
@@ -1,13 +1,16 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateItemDTO {
 
   @IsNotEmpty()
+  @IsString()
   title: string;
 
   @IsNotEmpty()
+  @IsString()
   body: string;
 
   @IsNotEmpty()
+  @IsString()
   deletePassword: string;
 }
